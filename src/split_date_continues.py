1a7f1287cccd7519dc8202a1c049a363f1111443
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 10:11:51 -0400

    init

---
+++ src/split_date_continues.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def split_date():
+    pass
+
+def split_date_continues():
+    pass
+
+def main():
+    df = split_date_continues()
+    print("Shape:", df.shape)
+    print("Column names:\n", df.columns)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
