22de226ca9a57b9b0bf14db88105e59d9b8e5408
modengann <99417989+modengann@users.noreply.github.com>
2023-10-31 13:22:39 -0400

    Update split_date_continues.py

--- src/split_date_continues.py
+++ src/split_date_continues.py
@@ -4,45 +4,23 @@ import pandas as pd
 
 
 def split_date():
-   df = pd.read_csv("src\Helsingin_pyorailijamaarat.csv", sep = ";")
-    df = df.dropna(how = "all")
-    df = df.dropna(axis = 1, how = "all")
-    split_days = df["Päivämäärä"].str.split(expand = True)
-    split_days.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
-    
-    weekdays = {
-        "ma":"Mon",
-        "ti":"Tue",
-        "ke":"Wed",
-        "to":"Thu",
-        "pe":"Fri",
-        "la":"Sat",
-        "su":"Sun"
+    df = pd.read_csv("src\Helsingin_pyorailijamaarat.csv", sep = ";")
+    df.dropna(how = "all", axis = 1, inplace = True)
+    df.dropna(how = "all", inplace = True)
+    split_data = df["Päivämäärä"].str.split(expand=True)
+    split_data.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    split_data["Hour"] = split_data["Hour"].str[0:2]
+    day_mapper = {"ma" : "Mon", "ti" : "Tue", "ke" : "Wed", 
+                  "to" : "Thu", "pe" : "Fri", "la" : "Sat", "su" : "Sun"}
+    split_data.replace(day_mapper, inplace=True)
+    month_mapper = {
+        "tammi": 1, "helmi" : 2, "maalis" : 3, "huhti" : 4, "touko" : 5,
+        "kesä" : 6, "heinä" : 7, "elo" : 8, "syys" : 9, "loka" : 10, "marras" : 11,
+        "joulu" : 12
     }
-    
-    months = {
-        "tammi" : "1",
-        "helmi" : "2",
-        "maalis" : "3",
-        "huhti" : " 4",
-        "touko" : "5",
-        "kesä" : "6",
-        "heinä" : "7",
-        "elo" : "8",
-        "syys" : "9",
-        "loka" : "10",
-        "marras" : "11",
-        "joulu" : "12"
-    }
-    split_days = split_days.replace(weekdays)
-    split_days = split_days.replace(months)
-    
-    split_days["Hour"] = split_days["Hour"].str.slice(stop = 2)
-    
-    split_days = split_days.astype({"Month" : int, "Day" : int, "Year" : int, "Hour" : int})
-    
-    
-    return split_days
+    split_data.replace(month_mapper, inplace= True)
+    split_data = split_data.astype({"Day" : int, "Month" : int, "Year" : int, "Hour": int})
+    return split_data
 
 def split_date_continues():
     pass
